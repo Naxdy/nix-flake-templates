ffb2c39a55767686b0cd4075250948ce788e3993
Naxdy <naxdy@naxdy.org>
2025-05-07 07:40:29 +0200

    rust: add taplo formatter

--- rust/treefmt.nix
+++ rust/treefmt.nix
@@ -7,6 +7,7 @@
     edition = cargoToml.workspace.package.edition or cargoToml.package.edition;
   };
   programs.nixfmt.enable = true;
+  programs.taplo.enable = true;
   programs.typos = {
     enable = true;
     includes = [
